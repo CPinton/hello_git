a2cd8a0c724a727733d53ea043d9b3b624d47daf
PINTON Caroline <caroline.pinton@developpement-durable.gouv.fr>
2023-02-01 16:28:08 +0100

    Fusion des fichiers 1 et 2 dans le fichier 2.

--- scripts/script1.R
+++
@@ -1,8 +0,0 @@
-
-alpha = c(1,2,3,4,5)
-beta = c(100,200,300,400,500)
-
-if (length(alpha == length(beta))){
-  gamma = beta / alpha 
-  print(gamma)
-}
--- scripts/script2.R
+++ scripts/script2.R
@@ -9,3 +9,12 @@ if (sas %in% liste_infos) {
   print(FALSE)  
 }
 
+
+alpha = c(1,2,3,4,5)
+beta = c(100,200,300,400,500)
+
+if (length(alpha == length(beta))){
+  gamma = beta / alpha 
+  print(gamma)
+}
+
